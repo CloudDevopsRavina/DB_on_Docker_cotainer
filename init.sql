cb6a75d7cd5d1c8575d11dfe3791417b9892359c
Ravina Rathod <ravinarathod009@gmail.com>
2026-03-25 13:16:13 +0530

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (username, email) VALUES
+('ravina', 'ravinarathod@gmail.com'),
+('naresh', 'rathod@gmail.com');
